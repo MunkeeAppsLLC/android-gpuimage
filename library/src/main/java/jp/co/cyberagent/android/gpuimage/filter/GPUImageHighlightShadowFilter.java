ef641460142625172cf5ba2cad8dfd954c0d97f2
Andrei <>
2019-12-10 12:08:20 -0700

    Update the HighlightShadow Filter to a more iOS like impl
    https://stackoverflow.com/questions/26511037/how-can-i-modify-this-webgl-fragment-shader-to-increase-brightness-of-highlights

--- library/src/main/java/jp/co/cyberagent/android/gpuimage/filter/GPUImageHighlightShadowFilter.java
+++ library/src/main/java/jp/co/cyberagent/android/gpuimage/filter/GPUImageHighlightShadowFilter.java
@@ -25,25 +25,38 @@ import android.opengl.GLES20;
  */
 public class GPUImageHighlightShadowFilter extends GPUImageFilter {
     public static final String HIGHLIGHT_SHADOW_FRAGMENT_SHADER = "" +
-            " uniform sampler2D inputImageTexture;\n" +
-            " varying highp vec2 textureCoordinate;\n" +
-            "  \n" +
-            " uniform lowp float shadows;\n" +
-            " uniform lowp float highlights;\n" +
-            " \n" +
-            " const mediump vec3 luminanceWeighting = vec3(0.3, 0.3, 0.3);\n" +
-            " \n" +
-            " void main()\n" +
-            " {\n" +
-            " 	lowp vec4 source = texture2D(inputImageTexture, textureCoordinate);\n" +
-            " 	mediump float luminance = dot(source.rgb, luminanceWeighting);\n" +
-            " \n" +
-            " 	mediump float shadow = clamp((pow(luminance, 1.0/(shadows+1.0)) + (-0.76)*pow(luminance, 2.0/(shadows+1.0))) - luminance, 0.0, 1.0);\n" +
-            " 	mediump float highlight = clamp((1.0 - (pow(1.0-luminance, 1.0/(2.0-highlights)) + (-0.8)*pow(1.0-luminance, 2.0/(2.0-highlights)))) - luminance, -1.0, 0.0);\n" +
-            " 	lowp vec3 result = vec3(0.0, 0.0, 0.0) + ((luminance + shadow + highlight) - 0.0) * ((source.rgb - vec3(0.0, 0.0, 0.0))/(luminance - 0.0));\n" +
-            " \n" +
-            " 	gl_FragColor = vec4(result.rgb, source.a);\n" +
-            " }";
+            "uniform sampler2D inputImageTexture;\n" +
+            "varying highp vec2 textureCoordinate;\n" +
+            "\n" +
+            "uniform lowp float shadows;\n" +
+            "uniform lowp float highlights;\n" +
+            "\n" +
+            "const mediump vec3 luminanceWeighting = vec3(0.2125, 0.7154, 0.0721);\n" +
+            "\n" +
+            "void main()\n" +
+            "{\n" +
+            "    lowp vec4 source = texture2D(inputImageTexture, textureCoordinate);\n" +
+            "    mediump float luminance = dot(source.rgb, luminanceWeighting);\n" +
+            "\n" +
+            "    //(shadows+1.0) changed to just shadows:\n" +
+            "    mediump float shadow = clamp((pow(luminance, 1.0/shadows) + (-0.76)*pow(luminance, 2.0/shadows)) - luminance, 0.0, 1.0);\n" +
+            "    mediump float highlight = clamp((1.0 - (pow(1.0-luminance, 1.0/(2.0-highlights)) + (-0.8)*pow(1.0-luminance, 2.0/(2.0-highlights)))) - luminance, -1.0, 0.0);\n" +
+            "    lowp vec3 result = vec3(0.0, 0.0, 0.0) + ((luminance + shadow + highlight) - 0.0) * ((source.rgb - vec3(0.0, 0.0, 0.0))/(luminance - 0.0));\n" +
+            "\n" +
+            "    // blend toward white if highlights is more than 1\n" +
+            "    mediump float contrastedLuminance = ((luminance - 0.5) * 1.5) + 0.5;\n" +
+            "    mediump float whiteInterp = contrastedLuminance*contrastedLuminance*contrastedLuminance;\n" +
+            "    mediump float whiteTarget = clamp(highlights, 1.0, 2.0) - 1.0;\n" +
+            "    result = mix(result, vec3(1.0), whiteInterp*whiteTarget);\n" +
+            "\n" +
+            "    // blend toward black if shadows is less than 1\n" +
+            "    mediump float invContrastedLuminance = 1.0 - contrastedLuminance;\n" +
+            "    mediump float blackInterp = invContrastedLuminance*invContrastedLuminance*invContrastedLuminance;\n" +
+            "    mediump float blackTarget = 1.0 - clamp(shadows, 0.0, 1.0);\n" +
+            "    result = mix(result, vec3(0.0), blackInterp*blackTarget);\n" +
+            "\n" +
+            "    gl_FragColor = vec4(result, source.a);\n" +
+            "}\n";
 
     private int shadowsLocation;
     private float shadows;
