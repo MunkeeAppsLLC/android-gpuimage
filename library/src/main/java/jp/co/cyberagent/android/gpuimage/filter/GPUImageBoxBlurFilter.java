df96329996b0d3df7918b1a1e122f1595a27ea4c
Andrei <andrei.verdes@shiftstudio.com>
2020-05-18 18:12:09 +0300

    Migrate and Split Mosaique into separate modules
    - core - core component, facade for GpuImage
    - made-ui - UI elements according to Made specs
    - made-templates - Read and inflate Made templates
    - instasize-ui - TBD

--- library/src/main/java/jp/co/cyberagent/android/gpuimage/filter/GPUImageBoxBlurFilter.java
+++ library/src/main/java/jp/co/cyberagent/android/gpuimage/filter/GPUImageBoxBlurFilter.java
@@ -104,6 +104,10 @@ public class GPUImageBoxBlurFilter extends GPUImageTwoPassTextureSamplingFilter 
         initTexelOffsets();
     }
 
+    public float getBlurSize() {
+        return this.blurSize;
+    }
+
     @Override
     public float getVerticalTexelOffsetRatio() {
         return blurSize;
